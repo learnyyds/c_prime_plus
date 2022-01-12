14b8ea5a71e5faefc65b8ba8cf2ae5e596a2d78b
likeyou <lky@likeyou.com>
2022-01-12 13:09:50 +0800

    笔记.doc更新

--- c_prime_plus示例代码/lky/书本代码实验.c
+++ c_prime_plus示例代码/lky/书本代码实验.c
@@ -15,7 +15,7 @@ int main(void)
 } 
 #endif
 
-
+#if 0 
 int main(void)
 {
 	float inch,cm;
@@ -25,6 +25,48 @@ int main(void)
 	printf("µÈÓÚ %0.2f ÀåÃ×",cm); 
 	return 0;	
 } 
+#endif
 
+#if 0 
+int main(void)
+{
+	printf("char:%d\r\n", sizeof(char));
+	printf("short:%d\r\n", sizeof(short));
+	printf("int:%d\r\n", sizeof(int));
+	printf("float:%d\r\n", sizeof(float));
+	printf("double:%d\r\n", sizeof(double));
+	printf("long:%d\r\n", sizeof(long));
+	printf("long long:%d\r\n", sizeof(long long));
+	printf("long double:%d\r\n", sizeof(long double));
 
+	printf("unsigned char:%d\r\n", sizeof(unsigned char));
+	printf("unsigned short:%d\r\n", sizeof(unsigned short));
+	printf("unsigned int:%d\r\n", sizeof(unsigned int));
+	printf("unsigned long:%d\r\n", sizeof(unsigned long));
+	printf("unsigned long long:%d\r\n", sizeof(unsigned long long));
+
+	printf("void * :%d\r\n", sizeof(void *));
+	printf("wchar_t:%d\r\n", sizeof(wchar_t));
+	return 0;
+}
+#endif
+ 
+
+int main(void)
+{
+
+    float a = 12.3;
+
+//    printf("%x\n", a);
+//    printf("%X\n", a);
+//    printf("%#x\n", a);
+//    printf("%#X\n", a);
+//    printf("%o\n", a);
+//	printf("%#o\n", a);
+//	printf("%hu\n", a);
+//	printf("%hhu\n", a);
+	printf("%F\n",a);
+    return 0;
+
+}
 
